8ab426192f1aac7a831f6b232e6dcaa9d71842f2
Ben Black <weepingwillowben@reed.edu>
2016-04-12 17:47:38 -0700

    test begun

--- time_cache.cpp
+++ time_cache.cpp
@@ -1,7 +1,14 @@
 #include "cache.h"
+#include <string.h>
 
-
+constexpr int maxmem = 10000;
 
 int main(){
-    
+    cache_t cache = create_cache(maxmem,NULL);
+    key_type k = (key_type)("hi there");
+    val_type v = (val_type)("valueable");
+    cache_set(cache,k,v,strlen(v)+1);
+    uint32_t vs = 0;
+    cache_get(cache,k,&vs);
+    return 0;
 }
